ce850c93bb16e333be6bd5de2062b3450084d404
Taylor Doyle <tcd56@cornell.edu>
2019-04-03 17:03:32 +0000

    Automatically backed up by Learn

--- spec/introduction_spec.rb
+++ spec/introduction_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
-describe "#introduction" do 
-  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do 
+describe "#introduction" do
+  it "takes in an argument of a name and puts out a phrase with that name using string interpolation" do
     expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
   end
 end
 
-describe "#introduction_with_language" do 
+describe "#introduction_with_language" do
   it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
   end
 end
